9d0d5dff36ee34dccc7f8ec01c9c76621c0b7aef
Nicholas Mackey <59402742+nikmackey@users.noreply.github.com>
2021-01-25 06:27:05 -0600

    feat: adds handle modal handle open/close

--- gallery.js
+++ gallery.js
@@ -1,11 +1,49 @@
 function Gallery(gallery) {
     if (!gallery) {
-        throw new Error('No Gallery Found!')
+      throw new Error('No Gallery Found!');
+    }
+    // select the elements we need
+    const images = Array.from(gallery.querySelectorAll('img'));
+    const modal = document.querySelector('.modal');
+    const prevButton = modal.querySelector('.prev');
+    const nextButton = modal.querySelector('.next');
+    let currentImage;
+  
+    function openModal() {
+      console.info('Opening Modal...');
+      // First check if the modal is already open
+      if (modal.matches('.open')) {
+        console.info('Madal already open');
+        return; // stop the function from running
+      }
+      modal.classList.add('open');
+  
+      // Event listeners to be bound when we open the modal:
+      window.addEventListener('keyup', handleKeyUp);
+      nextButton.addEventListener('click', showNextImage);
+      prevButton.addEventListener('click', showPrevImage);
     }
 
-    // select the elements
-    const images = Array.from(gallery.querySelectorAll('img'));
-    console.log(images)
+    function closeModal() {
+        modal.classList.remove('open');
+    }
+
+    function showImage(el) {
+        if (!el) {
+          console.info('no image to show');
+          return;
+        }
+        // update the modal with this info
+        console.log(el);
+        modal.querySelector('img').src = el.src;
+        modal.querySelector('h2').textContent = el.title;
+        modal.querySelector('figure p').textContent = el.dataset.description;
+        currentImage = el;
+        openModal();
+      }
+
+
+    images.forEach(image => image.addEventListener('click', (e) => showImage(e.currentTarget)));
 }
 
 // Use it on the page
