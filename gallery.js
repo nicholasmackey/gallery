110516538d5a52f0ba85aa85dffd56ac0cc2c69d
Nicholas Mackey <59402742+nikmackey@users.noreply.github.com>
2021-01-26 04:56:02 -0600

    feat: adds show prev/next image

--- gallery.js
+++ gallery.js
@@ -10,19 +10,24 @@ function Gallery(gallery) {
     let currentImage;
   
     function openModal() {
-      console.info('Opening Modal...');
       // First check if the modal is already open
       if (modal.matches('.open')) {
-        console.info('Madal already open');
         return; // stop the function from running
       }
       modal.classList.add('open');
-    }
       // Event listeners to be bound when we open the modal:
-      function closeModal() {
-          modal.classList.remove('open');
-          // TODO: add event listeners for keyboard
-      }
+      window.addEventListener('keyup', handleKeyUp);
+      nextButton.addEventListener('click', showNextImage);
+      prevButton.addEventListener('click', showPrevImage);
+    }
+
+
+    function closeModal() {
+        modal.classList.remove('open');
+        // TODO: add event listeners for keyboard
+        window.removeEventListener('keyup', handleKeyUp);
+        nextButton.removeEventListener('click', showNextImage);
+    }
 
     function handleClickOutside(e) {
         if (e.target === e.currentTarget) {
@@ -34,25 +39,31 @@ function Gallery(gallery) {
         if (e.key === 'Escape') closeModal();
     }
 
+    function showNextImage() {
+        showImage(currentImage.nextElementSibling || gallery.firstElementChild);
+    }
+
+    function showPrevImage() {
+        showImage(currentImage.previousElementSibling || gallery.lastElementChild);
+    }
+
     function showImage(el) {
         if (!el) {
           console.info('no image to show');
           return;
         }
         // update the modal with this info
-        console.log(el);
         modal.querySelector('img').src = el.src;
         modal.querySelector('h2').textContent = el.title;
         modal.querySelector('figure p').textContent = el.dataset.description;
         currentImage = el;
         openModal();
-      }
+    }
 
 
     images.forEach(image => image.addEventListener('click', (e) => showImage(e.currentTarget)));
 
     modal.addEventListener('click', handleClickOutside);
-    window.addEventListener('keyup', handleKeyUp);
 }
 
 // Use it on the page
